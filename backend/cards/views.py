a75630550711e05e7a3c2121c159cf4bd04baf2b
Decheng Zheng <dechengz@bu.edu>
2026-08-06 20:38:42 -0400

    add DRF Card serializer, list view, and URL routing

--- backend/cards/views.py
+++ backend/cards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Card
+from .serializers import CardSerializer
 
-# Create your views here.
+
+class CardListView(generics.ListAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
