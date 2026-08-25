a06d6823c7eda300fdc1c206ec27b6e066cfde90
Decheng Zheng <dechengz@bu.edu>
2026-08-24 22:59:25 -0400

    order PriceSnapshot queryset chronologically

--- backend/pricing/views.py
+++ backend/pricing/views.py
@@ -7,4 +7,4 @@ class PriceSnapshotListView(generics.ListAPIView):
 
     def get_queryset(self):
         card_id = self.kwargs["card_id"]
-        return PriceSnapshot.objects.filter(card_id=card_id)   
+        return PriceSnapshot.objects.filter(card_id=card_id).order_by("recorded_at") 
